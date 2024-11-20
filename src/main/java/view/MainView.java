604479d2d5bddad0ffab6fab42b505a5fd485661
JoseVitor2510 <[josevitorteofilosl@gmail.com](mailto:josevitorteofilosl@gmail.com)>
2024-11-20 16:01:04 -0300

    style: ajuste da formatação e melhora da aparência da interface

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -2,44 +2,62 @@ package view;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class MainView extends Application {
 
+   
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Tela Principal");
+        primaryStage.setTitle("Empréstimo Ferramentas");
 
         // Layout principal
-        VBox layout = new VBox(10);
+        VBox layout = new VBox(15);
         layout.setPadding(new Insets(20));
-
+        layout.setAlignment(Pos.CENTER);
+        
+      
+         // Titulo
+        Label titulo = new Label("Empréstimo Ferramentas");
+        titulo.setFont(Font.font("Arial",FontWeight.BOLD,20));
+        titulo.setTextFill(Color.BLACK);
+        
         // Botões para escolher a view
         Button btnAmigos = new Button("Gerenciar Amigos");
         Button btnFerramentas = new Button("Gerenciar Ferramentas");
 
+        
+        // Botões estilizados
+        btnAmigos.setStyle("-fx-background-color: #00FFFF; -fx-text-fill: black; -fx-font-size: 14px; -fx-padding: 10px;");
+        btnFerramentas.setStyle("-fx-background-color: #00CED1; -fx-text-fill: black; -fx-font-size: 14px; -fx-padding: 10px;");
+     
         // Ação do botão de Amigos
         btnAmigos.setOnAction(e -> {
             // Chama a View de Amigos
             new AmigoView(primaryStage).start(new Stage());
-            primaryStage.hide(); // Oculta a tela principal
+            primaryStage.hide();
         });
 
         // Ação do botão de Ferramentas
         btnFerramentas.setOnAction(e -> {
-            // Chama a View de Ferramentas
+
             new FerramentaView(primaryStage).start(new Stage());
             primaryStage.hide(); // Oculta a tela principal
         });
 
-        // Adiciona os botões ao layout
-        layout.getChildren().addAll(btnAmigos, btnFerramentas);
+        // Adiciona os botões e o TITULO ao layout 
+        layout.getChildren().addAll(titulo, btnAmigos, btnFerramentas);
 
         // Configura a cena e exibe
-        Scene scene = new Scene(layout, 300, 200);
+        Scene scene = new Scene(layout, 450, 250);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
