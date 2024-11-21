13509b363d073fd1b1b93d3642d8cea9be20da99
JoseVitor2510 <[josevitorteofilosl@gmail.com](mailto:josevitorteofilosl@gmail.com)>
2024-11-21 01:21:13 -0300

    feat: adicionado botão de Emprestimos na MainView

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -14,7 +14,6 @@ import javafx.stage.Stage;
 
 public class MainView extends Application {
 
-   
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Empréstimo Ferramentas");
@@ -23,25 +22,25 @@ public class MainView extends Application {
         VBox layout = new VBox(15);
         layout.setPadding(new Insets(20));
         layout.setAlignment(Pos.CENTER);
-        
-      
-         // Titulo
+
+        // Titulo
         Label titulo = new Label("Empréstimo Ferramentas");
-        titulo.setFont(Font.font("Arial",FontWeight.BOLD,20));
+        titulo.setFont(Font.font("Arial", FontWeight.BOLD, 20));
         titulo.setTextFill(Color.BLACK);
-        
+
         // Botões para escolher a view
         Button btnAmigos = new Button("Gerenciar Amigos");
         Button btnFerramentas = new Button("Gerenciar Ferramentas");
+        Button btnEmprestimos = new Button("Gerenciar Empréstimos");
 
-        
         // Botões estilizados
         btnAmigos.setStyle("-fx-background-color: #00FFFF; -fx-text-fill: black; -fx-font-size: 14px; -fx-padding: 10px;");
         btnFerramentas.setStyle("-fx-background-color: #00CED1; -fx-text-fill: black; -fx-font-size: 14px; -fx-padding: 10px;");
-     
+        btnEmprestimos.setStyle("-fx-background-color: #00CED3; -fx-text-fill: black; -fx-font-size: 14px; -fx-padding: 10px;");
+
         // Ação do botão de Amigos
         btnAmigos.setOnAction(e -> {
-            // Chama a View de Amigos
+
             new AmigoView(primaryStage).start(new Stage());
             primaryStage.hide();
         });
@@ -50,11 +49,17 @@ public class MainView extends Application {
         btnFerramentas.setOnAction(e -> {
 
             new FerramentaView(primaryStage).start(new Stage());
-            primaryStage.hide(); // Oculta a tela principal
+            primaryStage.hide();
+        });
+
+        btnEmprestimos.setOnAction(e -> {
+
+            new EmprestimoView(primaryStage).start(new Stage());
+            primaryStage.hide();
         });
 
         // Adiciona os botões e o TITULO ao layout 
-        layout.getChildren().addAll(titulo, btnAmigos, btnFerramentas);
+        layout.getChildren().addAll(titulo, btnAmigos, btnFerramentas, btnEmprestimos);
 
         // Configura a cena e exibe
         Scene scene = new Scene(layout, 450, 250);
